766b9a7b7316dabc7233d3f59e66666274ce0a39
persan <per.s.sandberg@bahnhof.se>
2016-09-14 07:14:59 +0200

    More prelemenary work

--- src.tests/cups-tests-main.adb
+++ src.tests/cups-tests-main.adb
@@ -1,4 +1,33 @@
+with Cairo.PDF;
+with cairo.Surface;
+with Ada.Command_Line;
+with Ada.Text_IO;
 procedure Cups.Tests.Main is
+   Tmpfilename : constant String := "/tmp/mo.pdf";
+   WIDTH    : constant := 595.0;
+   HEIGHT   : constant := 842.0;
+   Surface     : Cairo.Cairo_Surface := Cairo.PDF.Create
+     (Filename         => Tmpfilename,
+      Width_In_Points  => WIDTH,
+      Height_In_Points => HEIGHT);
+
+   Context   : Cairo.Cairo_Context := Cairo.Create (Surface);
+
 begin
-   null;
-end cups.tests.main;
+   -- Draw Some Text
+   Cairo.Select_Font_Face (Cr => Context,
+                           Family => "Arial Black",
+                           Slant => Cairo.Cairo_Font_Slant_Normal,
+                           Weight => Cairo.Cairo_Font_Weight_Normal);
+   Cairo.Set_Font_Size (Cr => Context, Size => 30.0);
+   Cairo.Show_Text (Cr => Context, Utf8 => Ada.Command_Line.Argument (1));
+
+
+   -- finish up
+   Cairo.Show_Page (Context);
+   Cairo.Destroy (Context);
+   Cairo.Surface.Flush (Surface);
+   Cairo.Surface_Destroy (Surface);
+   Ada.Text_IO.Put_Line (GetDefault);
+   CUPS.PrintFile (GetDefault, Tmpfilename, "cairo PS", 0);
+end Cups.Tests.Main;
--- src/cups.ads
+++ src/cups.ads
@@ -1,3 +1,15 @@
+with GNAT.Strings;
 package CUPS is
-   pragma Pure;
+
+   type Cups_Option is record
+      Name : GNAT.Strings.String_Access;
+      Value : GNAT.Strings.String_Access;
+   end record;
+   function GetDefault return String;
+   procedure PrintFile
+     (Arg1 : String;
+      Arg2 : String;
+      Arg3 : String;
+      Arg4 : Integer;
+      Arg5 : access Cups_Option := null);  -- cups/cups.h:370
 end CUPS;
