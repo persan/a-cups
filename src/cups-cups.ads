7efb54e1c1f1aef1bbd7757dfd2cd7e954dc9a5b
VSundin <victorsundin@hotmail.com>
2016-09-19 14:08:15 +0000

    Changed from the Options_Array to using the Cups_Options_T record due to problems with the AddOptions() function

--- src/cups-cups.ads
+++ src/cups-cups.ads
@@ -1,6 +1,17 @@
+with Interfaces.C;
+with Interfaces.C.Strings;
+with Ada.Containers;
 with Cups.String_Maps;
-package CUPS.CUPS is
+with CUPS.Cups_Cups_H;
+with System;
 
+private package CUPS.CUPS is
+   use Cups_Cups_H;
+   use Interfaces.C.Strings;
+   use Interfaces.C;
+   use Ada.Containers;
+
+   type Cups_Option is access Cups_Option_T;
 
    function GetDefault return String;
    --
@@ -13,15 +24,38 @@ package CUPS.CUPS is
    --  get the user-defined default printer,
    --  as this function does not support the lpoptions-defined default printer.
 
-
-
    function PrintFile
      (Name        : String;
       Filename    : String;
       Title       : String;
-      Options     : String_Maps.Map := String_Maps.Empty_Map) return Job_Id;
+      Num_Options : Job_Id;
+      Options     : Cups_Option) return Job_Id;
    --
    -- Print a file to a printer or class on the default server.
+
+
+   function CancelJob
+     (Name        : String;
+      JobId       : Integer := -1 ) return Job_Id;
+   --
+   -- Cancel a job in the queue
+   -- JobId -1 yields a termination of all jobs
+
+   function AddOption
+     (Name   : String;
+      Value  : String;
+      Num_Options : Job_Id;
+      Options     : System.Address) return Job_Id;
+   --
+   -- Add an option to an option array
+
+   function GetOption
+     (Name : String;
+      Num_Options : Job_Id;
+      Options     : Cups_Option) return String;
+   --
+   -- Get an Option Value or Null
+   -- For instance, "printer-state" tells if the printer is idle, processing etc..
 
 
 end CUPS.CUPS;
