117c163c425e60aca1add74e688ecd3b6986e193
VSundin <victorsundin@hotmail.com>
2016-09-21 08:02:56 +0200

    Changed the input argument from System.Address to Cups_Option

--- src.pinprint/cups-pinprint-main.adb
+++ src.pinprint/cups-pinprint-main.adb
@@ -15,14 +15,14 @@ procedure CUPS.Pinprint.Main is -- Main, but no special name is needed.
    use Interfaces.C;
    use Cups_Cups_H;
 
-   Num_Options : Job_Id := 0;
-   Printer_State : Standard.String := "";
-    Id      : Job_Id := 0;
-    Cancel_Job    : Job_Id := 0;
+   Num_Options    : Job_Id          := 0;
+   Printer_State  : Standard.String := "";
+   Id             : Job_Id          := 0;
+   Cancel_Job     : Job_Id          := 0;
 
-    Options       : Cups.Cups_Option;
-    Temp_File   : File_Type;
-    Temp_File_Name : Standard.String := "TempFile.txt";
+   Options        : aliased Cups.Cups_Option;
+   Temp_File      : File_Type;
+   Temp_File_Name : Standard.String := "TempFile.txt";
 
 begin
    --Printer_State := Cups.GetOption ("printer-state", Options'Address);
@@ -31,37 +31,37 @@ begin
    -- If printer is active, print jobs
    --if Printer_State /= "" then
    --if Integer'Value(Printer_State) < 3 then
-         --Put_Line (Printer_State);
-         Num_Options := Cups.AddOption ("raw", "true", Num_Options, Options'Address);
-         Num_Options := Cups.AddOption ("usb-no-reattach-default", "true", Num_Options, Options'Address);
+   --Put_Line (Printer_State);
+   Num_Options := Cups.AddOption ("raw", "true", Num_Options, Options);
+   Num_Options := Cups.AddOption ("usb-no-reattach-default", "true", Num_Options, Options);
 
 
-         -- Create the file for printing
-         Create (Temp_File, Out_File, Temp_File_Name);
-         Put_Line (Temp_File, "Hi!");
-         Put_Line (Temp_File, "Å");
-         Put_Line (Temp_File, "Ä");
-         Put_Line (Temp_File, "Ö");
-         Put_Line (Temp_File, "Bye!");
-         -- Close before sending to printer
-         Close (Temp_File);
+   -- Create the file for printing
+   Create (Temp_File, Out_File, Temp_File_Name);
+   Put_Line (Temp_File, "Hi!");
+   Put_Line (Temp_File, "Å");
+   Put_Line (Temp_File, "Ä");
+   Put_Line (Temp_File, "Ö");
+   Put_Line (Temp_File, "Bye!");
+   -- Close before sending to printer
+   Close (Temp_File);
 
-         -- Print file
-         Id := Cups.PrintFile (Cups.GetDefault, Temp_File_Name, "Test Print", Num_Options, Options);
-         if Id /= 0 then
-            Put_Line ("Printing!");
-         else
-            Put_Line ("Printing failed!");
-         end if;
+   -- Print file
+   Id := Cups.PrintFile (Cups.GetDefault, Temp_File_Name, "Test Print", Num_Options, Options);
+   if Id /= 0 then
+      Put_Line ("Printing!");
+   else
+      Put_Line ("Printing failed!");
+   end if;
 
 
-      --Cancel all Jobs otherwise
-      --else
---         Cancel_Job := Cups.CancelJob (Cups.GetDefault);
-         --Put_Line ("Printer not active!");
-      --end if;
+   --Cancel all Jobs otherwise
    --else
-      --Put_Line ("No printer found!");
+   --         Cancel_Job := Cups.CancelJob (Cups.GetDefault);
+   --Put_Line ("Printer not active!");
+   --end if;
+   --else
+   --Put_Line ("No printer found!");
    --end if;
 
 end Cups.Pinprint.Main;
