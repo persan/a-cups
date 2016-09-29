dcd5fc39ac54aa96d2e7f297173c68deb2d5e205
VSundin <victorsundin@hotmail.com>
2016-09-29 13:05:38 +0200

    Renamed, removed 'cups-'and made separate from the cups package

---
+++ src.pinprint/pinprint-main.adb
@@ -0,0 +1,11 @@
+with Cups.Cups;
+procedure Pinprint.Main is -- Main, but no special name is needed.
+begin
+   Cups.Cups.PrintString ("Hello", True);
+   --  Cups.PrintString ("Hi there!", True);
+   --  Cups.PrintString ("How are you?", True);
+   --  Cups.PrintString ("I am fine thank you!", True);
+   --  Cups.PrintString ("Bye!", True);
+
+
+end Pinprint.Main;
---
+++ src.pinprint/pinprint.ads
@@ -0,0 +1,3 @@
+package Pinprint is
+
+end Pinprint;
